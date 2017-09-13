4cd267d2e904bdfdb156c497ebd2ac4ad5ea0fc5
Dessie Kirrane <dkirrane@avaya.com>
2017-09-13 12:40:21 +0100

    Add log-courier

--- run.sh
+++ run.sh
@@ -41,4 +41,4 @@ echo "Starting filebeat"
 # docker-compose up -d filebeat
 # docker-compose run filebeat
 
-nc localhost 5000 < ./filebeat/test-logs/wa-impu_1_1.json.log
+nc localhost 5001 < ./filebeat/test-logs/wa-impu_1_1.json.log
